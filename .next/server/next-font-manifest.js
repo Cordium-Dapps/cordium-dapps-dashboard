1b06d763ff40911d8e6e5cfef59c92d7c5cb9807
Yakumwamba <brianlemba@gmail.com>
2023-09-06 17:02:24 +0200

    Fix font not found in index.css

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/yakumwamba/CodeWithYaku/grants/cordium-dapps/app/layout\":[\"static/media/560d0dc937e89811-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
